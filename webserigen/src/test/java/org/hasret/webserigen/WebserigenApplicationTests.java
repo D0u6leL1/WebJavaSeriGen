a04df97b2b01dda9358882d93dfc370174349a38
DoubleLii <doubleli@bupt.edu.cn>
2025-03-28 00:29:56 +0800

    test

---
+++ webserigen/src/test/java/org/hasret/webserigen/WebserigenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hasret.webserigen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserigenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
